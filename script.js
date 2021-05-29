7da02324fc31d60301db63623bb6d629874334f0
Agnieszka Wojtasik <agnieszkawojtasik@MacBook-Air-Agnieszka.local>
2021-05-29 11:23:35 +0200

    Changed html and js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// const { async } = require("regenerator-runtime");
-
-// const { async } = require("regenerator-runtime");
-
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
 const loader = document.querySelector('.loader');
